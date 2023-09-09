8bacecba5c3ae68a9ebc50c862fc9a9fcf02a016
Harshil jain <twitharshil@gmail.com>
2023-09-09 12:33:11 +0530

    Initial commit

---
+++ renderer.js
@@ -0,0 +1,36 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off. Use `preload.js` to
+// selectively enable features needed in the rendering
+// process.
+const button = document.getElementById('button-id');
+button.addEventListener('click', () => {
+  createBrowserWindow();
+});
+
+function createBrowserWindow() {
+  const { shell } = require('electron')
+  shell.openExternal('https://www.postman.com')
+}
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
